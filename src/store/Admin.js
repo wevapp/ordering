ead4922d8b91dd90a2abe16ec9a41ed37dc78303
wevapp <raymondestrella1993@gmail.com>
2023-12-07 10:16:32 +0800

    commit

---
+++ src/store/Admin.js
@@ -0,0 +1,12 @@
+import { create } from "zustand";
+
+const useAdminStore = create((set) => ({
+//   admin account
+admin: [{
+    username: 'akoSiR@ymond',
+    password: 'raymond!@#$%',
+}],
+
+}))
+
+export default useAdminStore
