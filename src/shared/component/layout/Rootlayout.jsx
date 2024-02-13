6a8a6611472276cd0dc6720164f4aff2e956a6cd
wevapp <raymondestrella1993@gmail.com>
2024-02-13 20:52:21 +0800

    update bg

--- src/shared/component/layout/Rootlayout.jsx
+++ src/shared/component/layout/Rootlayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 const Rootlayout = () => {
   return (
-    <div className='container h-[100%]' style={{ backgroundImage: `url('/src/images/BackgroundImage/bg-main.png')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', height: '100%', margin: 'auto', backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '100%'}}>
+    <div className='container h-[100%]' style={{ backgroundImage: `url('/public/img/bg-main.png')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', height: '100%', margin: 'auto', backgroundColor: 'rgba(0, 0, 0, 0.5)', width: '100%'}}>
       <Outlet />
     </div>
   )
