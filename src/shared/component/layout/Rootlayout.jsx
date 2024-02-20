9e3bc19327cac31e72a869317f63e7b291685760
wevapp <raymondestrella1993@gmail.com>
2024-02-20 11:19:57 +0800

    update

--- src/shared/component/layout/Rootlayout.jsx
+++ src/shared/component/layout/Rootlayout.jsx
@@ -13,18 +13,10 @@ const RootLayout = () => {
   };
 
   return (
-<<<<<<< HEAD
     <div style={backgroundStyle} className='container h-full'>
-=======
-    <div className='container h-[100%]' style={{ backgroundImage: `url('src/images/BackgroundImage/bg.PNG')`, backgroundSize: 'cover', backgroundPosition: 'center', backgroundRepeat: 'no-repeat', height: '100%', margin: 'auto', width: '100%'}}>
->>>>>>> 416fa8c8d88321bf12c6065b88b70e42053d3434
       <Outlet />
     </div>
   );
 };
 
-<<<<<<< HEAD
 export default RootLayout;
-=======
-export default Rootlayout
->>>>>>> 416fa8c8d88321bf12c6065b88b70e42053d3434
